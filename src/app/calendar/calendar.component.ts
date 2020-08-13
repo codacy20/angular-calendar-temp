0b4e57c761fb2cc8a8707406d02c81c195277a14
codacy20 <kmuc72@gmail.com>
2020-08-13 18:50:13 +0200

    calendar logic

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -7,19 +7,28 @@ import { Day, WeekDays } from "../model";
   styleUrls: ["./calendar.component.css"]
 })
 export class CalendarComponent implements OnInit {
+  DAY = 86400000;
   active = 2;
   days: Day[] = [
-    { name: WeekDays[0], date: 1 },
-    { name: WeekDays[1], date: 2 },
-    { name: WeekDays[2], date: 3 },
-    { name: WeekDays[3], date: 4 },
-    { name: WeekDays[4], date: 5 },
-    { name: WeekDays[5], date: 6 }
+    { name: this.getDay(-2), date: this.getDate(-2) },
+    { name: this.getDay(-1), date: this.getDate(-1) },
+    { name: this.getDay(0), date: this.getDate(0) },
+    { name: this.getDay(1), date: this.getDate(1) },
+    { name: this.getDay(2), date: this.getDate(2) },
+    { name: this.getDay(3), date: this.getDate(3) }
   ];
   constructor() {}
 
   getMessage(index: number) {
     this.active = index;
+  }
+
+  getDate(index: number) {
+    return new Date(Date.now() + index * this.DAY).getDate();
+  }
+
+  getDay(index: number) {
+    return WeekDays[new Date(Date.now() + index * this.DAY).getDay()];
   }
 
   ngOnInit() {}
