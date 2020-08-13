40593ce922608d51833c6e564fe146f9caabe789
codacy20 <kmuc72@gmail.com>
2020-08-13 15:19:44 +0200

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
 import { CalendarComponent } from './calendar/calendar.component';
 import { DayComponent } from './day/day.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent, CalendarComponent, DayComponent ],
+  declarations: [ AppComponent, CalendarComponent, DayComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
